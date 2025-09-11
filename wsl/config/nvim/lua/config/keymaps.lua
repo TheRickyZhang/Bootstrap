8c0eb8163077a9595ceff5f4e2e21a26a533bd4f
Ricky Zhang <ricky@example.com>
2025-09-11 00:28:53 -0400

    reconfig for wsl update

--- wsl/config/nvim/lua/config/keymaps.lua
+++ wsl/config/nvim/lua/config/keymaps.lua
@@ -7,58 +7,18 @@ map("n", "<C-u>", "<C-u>zz", { silent = true })
 map("n", "n", "nzzzv", { silent = true })
 map("n", "N", "Nzzzv", { silent = true })
 
--- Ctrl-/ → toggle comment (Comment.nvim must be installed)
-local ok, comment = pcall(require, "Comment.api")
-if ok then
-  for _, mode in ipairs({ "n", "v" }) do
-    map(mode, "<C-_>", function()
-      if mode == "n" then
-        comment.toggle.linewise.current()
-      else
-        comment.toggle.linewise(vim.fn.visualmode())
-      end
-    end, { silent = true })
-  end
+local ok, api = pcall(require, "Comment.api")
+if not ok then
+  return
 end
 
--- Alt-j/k → move lines up/down
-map("n", "<A-j>", ":m .+1<CR>==", { silent = true })
-map("n", "<A-k>", ":m .-2<CR>==", { silent = true })
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
-
-local motions = {
-  ["<A-w>"] = "/\\u<CR>", -- next Uppercase
-  ["<A-b>"] = "?\\u<CR>", -- prev Uppercase
-  ["<A-e>"] = "/\\u<CR>h", -- end of camel-part
-  ["<A-g>e"] = "?\\u<CR>l", -- “ge”: Alt-g then e
-}
-
-for lhs, rhs in pairs(motions) do
-  for _, mode in ipairs({ "n", "o", "x" }) do
-    map(mode, lhs, rhs, { silent = true, noremap = true })
-  end
-end
-
--- Insert mode commands
-map("n", "<leader>gs", vim.lsp.buf.signature_help, {
-  silent = true,
-  desc = "Signature help",
-})
-
-map("i", "<C-g>", function()
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-k>", true, false, true), "n", false)
-end, { desc = "Insert digraph" })
-
-map("i", "<C-i>", function()
-  local keys = vim.api.nvim_replace_termcodes("<Esc><leader>iA", true, false, true)
-  vim.api.nvim_feedkeys(keys, "m", false) -- "m" = allow remap
-end)
-
-map("i", "<A-k>", function()
-  require("blink.cmp").show()
+map("n", "<C-_>", api.toggle.linewise.current, { silent = true })
+map("x", "<C-_>", function()
+  api.toggle.linewise(vim.fn.visualmode())
 end, { silent = true })
 
+-- Legend: * = in chord, + = added custom to a chord, # = custom
+------------------------------ Normal mode commands  ------------------------------
 -- Letter status for leader (* = prefix for other things already in lazy, # = self defined, + = adding to existing prefix)
 -- Ctrl - tmux
 -- a  - copilot
@@ -69,16 +29,16 @@ end, { silent = true })
 -- f* - snacks find
 -- g+ - git
 -- h  - harpoon
--- i# - math with typst
+-- i  -
 -- j  -
 -- k  -
 -- l  - lazy
 -- m# - math in typst via $
--- n  - notifications
+-- n  - notifications             (May be overriden for math $$ in typst)
 -- o  - show all in blink
--- p# - typst preview (overwrites view yank history)
--- q  - nvim persistence (TODO learn how to use this)
--- r  - primagen refactor (TODO learn how to use this)
+-- p# - typst preview             (overwrites view yank history)
+-- q  - nvim persistence          (TODO learn how to use this)
+-- r  - primagen refactor         (TODO learn how to use this)
 -- s* - search + various
 -- t+ - (overwrites neotest, TODO not installed yet)
 -- u  - toggle ui elements
@@ -88,12 +48,9 @@ end, { silent = true })
 -- y# - quick spell fix (z=1)
 -- z# - Blink insertion suggestions
 
--- Insert mode mappings that are not available: c, d, g(digraphs), h, i, j, k, m, n, p, r, s (remapped to signature), t,  w
---
-
--- Add math mode for typst with surrounding $
-map("n", "<leader>i", "gsaiW$", {
-  remap = true,
+map("n", "<leader>gs", vim.lsp.buf.signature_help, {
+  silent = true,
+  desc = "Signature help",
 })
 
 map("n", "<leader>k", function()
@@ -117,7 +74,6 @@ map(
   { silent = true, desc = "Typst → same dir" }
 )
 
--- map("n", "<leader>y", "Y$%", { silent = true })
 map("n", "<leader>v", "V$%", { silent = true })
 
 map("n", "<leader>y", "1z=")
@@ -125,8 +81,64 @@ map("n", "<leader>y", "1z=")
 -- <leader>d → delete into black hole
 -- map({ "n", "v" }, "<leader>d", '"_d', { silent = true })
 
+------------------------------ Insert mode commands  ------------------------------
+-- Insert Mode Mappings
+-- a  - text insertion
+-- b  -
+-- c  - Esc
+-- d  - deindent
+-- e  - Insert below character
+-- f  - snacks find
+-- g  - Override current file summary
+-- h  - Backspace
+-- i  - Tab
+-- j  - Enter
+-- k  - Signature help (Override digraph)
+-- l+ - literal digraph
+-- m  - Enter
+-- n  - Backspace
+-- o  - One normal mode
+-- p? - Backwards keyword completion?
+-- q  - Insert literal ^X
+-- r  - Register
+-- s  - Save
+-- t+ - Tab
+-- u  - Delete to line begin
+-- v# - Paste
+-- w  - Delete word back
+-- x  - Super special mode for niche things?
+-- y  - Insert the above character (Currently overriden by blink)
+-- z  -
+
+map("i", "<C-l>", function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-k>", true, false, true), "n", false)
+end, { desc = "Insert digraph" })
+
+-- map("i", "<A-k>", function()
+--   require("blink.cmp").show()
+-- end, { silent = true })
+
 -- Visual mode commands
 
 map("v", "p", '"_dP', { silent = true })
 map("v", "*", 'y/\\V<C-R>"<CR>', { silent = true })
 map("v", "#", 'y?\\V<C-R>"<CR>', { silent = true })
+
+-- Alt-j/k → move lines up/down
+map("n", "<A-j>", ":m .+1<CR>==", { silent = true })
+map("n", "<A-k>", ":m .-2<CR>==", { silent = true })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
+
+local motions = {
+  ["<A-w>"] = "/\\u<CR>", -- next Uppercase
+  ["<A-b>"] = "?\\u<CR>", -- prev Uppercase
+  ["<A-e>"] = "/\\u<CR>h", -- end of camel-part
+  ["<A-g>e"] = "?\\u<CR>l", -- “ge”: Alt-g then e
+}
+
+for lhs, rhs in pairs(motions) do
+  for _, mode in ipairs({ "n", "o", "x" }) do
+    map(mode, lhs, rhs, { silent = true, noremap = true })
+  end
+end
