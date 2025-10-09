a5d47e2838599f630b92a3604e761f2c602ea5b3
Ricky Zhang <rickyzhang196@outlook.com>
2025-10-09 02:31:44 -0400

    adjust print commands, chrome settings

--- configCopy/nvim/ftplugin/typst.lua
+++ configCopy/nvim/ftplugin/typst.lua
@@ -2,9 +2,12 @@
 local map = vim.keymap.set
 
 map("n", "<leader>m", "gsaiW$", { remap = true, buffer = true })
-map("x", "<leader>m", "gsa$", { remap = true, buffer = true })
+map("n", "<C-m>", "gsaiW$", { remap = true, buffer = true })
 
--- map("i", "<C-y>", function()
---   local keys = vim.api.nvim_replace_termcodes("<Esc><leader>yA", true, false, true)
---   vim.api.nvim_feedkeys(keys, "m", false)
--- end, { buffer = true })
+map("x", "<leader>m", "gsa$", { remap = true, buffer = true })
+map("x", "<C-m>", "gsa$", { remap = true, buffer = true })
+
+map("i", "<C-e>", function()
+  local keys = vim.api.nvim_replace_termcodes("<Esc><C-m>Ea", true, false, true)
+  vim.api.nvim_feedkeys(keys, "m", false)
+end, { buffer = true })
